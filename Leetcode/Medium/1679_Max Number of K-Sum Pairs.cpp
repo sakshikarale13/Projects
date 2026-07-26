82c3f897b64e9839c67ba3f961b177bb5f79be50
sakshikarale13 <sakshikarale34@gmail.com>
2026-07-26 17:36:29 +0530

    Create 1679_Max Number of K-Sum Pairs.cpp

---
+++ Leetcode/Medium/1679_Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k)
+    {
+        sort(nums.begin(),nums.end());
+        int count = 0;
+        int left=0;
+        int right = nums.size()-1;
+
+        while(left < right)
+        {
+            if(nums[left]+nums[right]==k)
+            {
+                left++;
+                right--;
+                count++;
+            }
+            else if(nums[left]+nums[right]<k)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return count;
+    }
+};
